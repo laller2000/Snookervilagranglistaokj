88b8559c9217232c5dcee6c06a13bce8866923a0
laller2000 <69633175+laller2000@users.noreply.github.com>
2021-03-08 17:52:15 +0100

    1.0verzio
    1.0verzio

---
+++ Snooker/Snooker/Snooker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Snooker
+{
+    class Snooker
+    {
+        static List<Vilagranglista> vilaglista = new List<Vilagranglista>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2.feladat:snooker.txt beolvasása");
+            Beolvasas("snooker.txt");
+            Console.WriteLine($"\n3.feladat:A világranglistán {vilaglista.Count} versenyző szerepel");
+            Console.WriteLine($"\n4.feladat:A verenyzők átlagosan {vilaglista.Average(a =>a.Nyeremeny) :N2} fontot kerestek");
+            Console.WriteLine("\n5.feladat:A legjobb kereső kínai versenyző");
+            int legjobbkinai = vilaglista.Where(a => a.Orszag.Equals("Kína")).Max( b => b.Nyeremeny);
+            Vilagranglista legjobb=vilaglista.Find(c => c.Nyeremeny==legjobbkinai);
+            Console.WriteLine($"\tHelyezés:{legjobb.Helyezes}");
+            Console.WriteLine($"\tNév:{legjobb.Nev}");
+            Console.WriteLine($"\tOrszág:{legjobb.Orszag}");
+            Console.WriteLine($"\tNyeremény összege:{(legjobb.Nyeremeny*380).ToString("#,##0")} FT");
+            string norveg = "";
+            if (vilaglista.Find( d => d.Orszag.Equals("Norvégia"))==null)
+            {
+                norveg = "A versenyzők között nincs norvég versenyző";
+            }
+            else
+            {
+                norveg = "A versenyzők között van norvég versenyző";
+            }
+            Console.WriteLine($"\n6.feladat:"+norveg);
+            Console.WriteLine("\n7.feladat:Statisztika");
+            var orszagok = vilaglista.GroupBy(a => a.Orszag).Select(b => new {o = b.Key , db= b.Count()}).Where(g =>g.db>4);
+            foreach (var item in orszagok)
+            {
+                Console.WriteLine($"\t{item.o}-{item.db} fő");
+            }
+            Console.WriteLine("\nProgram vége:");
+            Console.ReadLine();
+        }
+        static void Beolvasas(string filenev)
+        {
+            using (StreamReader sr=new StreamReader(filenev,Encoding.Default))
+            {
+                string fejlec = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                   
+                    Vilagranglista v = new Vilagranglista(sr.ReadLine());
+                    vilaglista.Add(v);
+                }
+                sr.Close();
+            }
+        }
+    }
+}
---
+++ Snooker/Snooker/Vilagranglista.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snooker
+{
+    class Vilagranglista
+    {
+        //Helyezes;Nev;Orszag;Nyeremeny
+        private int helyezes;
+        private string nev;
+        private string orszag;
+        private int nyeremeny;
+
+        public int Helyezes { get => helyezes; set => helyezes = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Orszag { get => orszag; set => orszag = value; }
+        public int Nyeremeny { get => nyeremeny; set => nyeremeny = value; }
+
+        public Vilagranglista(int helyezes, string nev, string orszag, int nyeremeny)
+        {
+            Helyezes = helyezes;
+            Nev = nev;
+            Orszag = orszag;
+            Nyeremeny = nyeremeny;
+        }
+        public Vilagranglista(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Helyezes = int.Parse(adatok[0]);
+            Nev = adatok[1];
+            Orszag = adatok[2];
+            Nyeremeny = int.Parse(adatok[3]);
+        }
+    }
+}
